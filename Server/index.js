27f22fb7122666f1a406288cd733293cf9276537
Daniyal9988 <sdani9988@gmail.com>
2024-04-04 04:57:22 -0700

    debugging left

--- Server/index.js
+++ Server/index.js
@@ -1,32 +1,44 @@
-const express = require("express"); // Import Express.js framework
-const http = require("http"); // Import Node.js built-in HTTP module
-const cors = require("cors"); // Import CORS middleware for enabling Cross-Origin Resource Sharing
-const SocketIO = require("socket.io"); // Import Socket.IO library for real-time bidirectional communication
+const http=require("http");
+const express =require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
 
-// Create an Express application
-const app = express();
-const port = 4500;
-
-
-app.use(cors()); // This allows requests from another domain
+const app=express();
+const port= 4500 ;
 
 
-app.get("/", (req, res) => {
-    res.send("its working");
+const users=[{}];
+
+app.use(cors());
+app.get("/",(req,res)=>{
+    res.send("HELL ITS WORKING");
+})
+
+const server=http.createServer(app);
+
+const io=socketIO(server);
+
+io.on("connection",(socket)=>{
+    console.log("New Connection");
+
+    socket.on('joined',({user})=>{
+          users[socket.id]=user;
+          console.log(`${user} has joined `);
+          socket.broadcast.emit('userJoined',{user:"Admin",message:` ${users[socket.id]} has joined`});
+          socket.emit('welcome',{user:"Admin",message:`Welcome to the chat,${users[socket.id]} `})
+    })
+
+    socket.on('message',({message,id})=>{
+        io.emit('sendMessage',{user:users[id],message,id});
+    })
+
+    socket.on('disconnect',()=>{
+          socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}  has left`});
+        console.log(`user left`);
+    })
 });
 
-// Create an HTTP server using the Express app
-const server = http.createServer(app);
 
-// Initialize Socket.IO by passing the HTTP server instance
-const io = SocketIO(server);
-
-// Listen for the "connection" event, which occurs when a new client connects to the server
-io.on("connection", () => {
-    console.log("new connection established");
-});
-
-// Start the server and listen for incoming requests on the specified port
-server.listen(port, () => {
-    console.log(`the server is listening to request on ${port}`);
-});
+server.listen(port,()=>{
+    console.log(`Working`);
+})
